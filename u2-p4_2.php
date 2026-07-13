57310d1d427bdf06fffc62e7bcce98e095604946
meetvaghasiya1810 <meetvaghasiya1810@gmail.com>
2026-07-13 11:58:24 +0530

    Add files via upload

---
+++ u2-p4_2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $str = "welcome to php";
+        echo "<hr><h3>2.strpos()</h3>";
+        echo "position of 'PHP' = ".strpos($str,"php");
+    ?>
+</body>
+</html>
